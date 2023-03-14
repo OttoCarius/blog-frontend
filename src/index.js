bcba5953dab35a9ce0c391c9a77836b7312653ac
OttoCarius <ottocarius1993@gmail.com>
2023-03-14 17:32:57 +0200

    fx

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Provider store={store}>
         <App />
       </Provider>
